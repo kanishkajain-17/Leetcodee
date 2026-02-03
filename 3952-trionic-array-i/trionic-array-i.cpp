d32740f0a0ee2b70a8ccde0af00690543b2a1d90
kanishkajain-17 <kanishkajn477@gmail.com>
2026-02-03 12:30:49 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (35.38%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.cpp
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -6,18 +6,19 @@ public:
             while( i + 1 < n && nums[i] < nums[i + 1]){
                 i++;
             }
-            if(i == 0 || i == n - 1)
+            if(i == 0 || i == n - 1) //agar sirf increasing he nahi hai tab
+                //aur poore mai sirf increasing he hai --> [1, 3, 5]
                 return false;
             while(i + 1 < n && nums[i] > nums[i + 1]){
                 i++;
             }
-            if(i == n - 1)
+            if(i == n - 1) //poore array mai sirf increasing or dec he hai vaps inc nahi hai 
                 return false;
             while(i + 1 < n && nums[i] < nums[i + 1]){
                 i++;
-            }
+            }//sab kuch hai 
         
-        if(i == n - 1)
+        if(i == n - 1)//last tak pahuch gaya inc + dec + inc
             return true;
         return false;
     }
