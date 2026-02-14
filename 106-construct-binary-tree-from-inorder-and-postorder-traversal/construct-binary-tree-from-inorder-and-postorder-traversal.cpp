ede71e29ee8723d17de5f521ec3bbec556df1bf8
kanishkajain-17 <kanishkajn477@gmail.com>
2026-02-14 13:30:42 +0530

    Time: 7 ms (28.30%) | Memory: 27.1 MB (85.30%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,38 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* solve(vector<int>& inorder, vector<int>& postorder, int start, int end, int & idx){
-        if(idx < 0)
+    TreeNode* buildBT(vector<int>& inorder, vector<int>& postorder, int inStart, int inEnd, int postStart, int postEnd) {
+        if(inStart > inEnd)
             return NULL;
-        if(start > end)
-            return NULL;
-        int rootVal = postorder[idx];
-        int i;
-        for(i = start; i <= end; i++){
-            if(inorder[i] == rootVal)
+
+        TreeNode* root = new TreeNode(postorder[postEnd]);
+        int root_candidate = root->val;
+        int i = inStart;
+      
+        //YOu can optimize this by storing index of inorder values in a map for direct access
+        for(; i<=inEnd; i++) {
+            if(inorder[i] == root_candidate) {
                 break;
+            }
         }
-        idx--; //preorder mai ek value aage badh gaye
-        TreeNode* root = new TreeNode(rootVal);
-
-        root->right = solve(inorder, postorder, i + 1, end, idx);
-        root->left = solve(inorder, postorder, start, i - 1, idx);
-
-        return root;     
+        int leftSize  = i-inStart;
+        int rightSize = inEnd-i;
+      
+        root->left = buildBT(inorder, postorder, inStart, i-1, postStart, postStart+leftSize-1);
+        root->right  = buildBT(inorder, postorder, i+1, inEnd, postEnd-rightSize, postEnd-1);
+      
+        return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int n = inorder.size();
-        int idx = n - 1;
-        return solve(inorder, postorder, 0, n-1, idx);
+        TreeNode* root = NULL;
+        int n          = postorder.size();
+        int inStart    = 0;
+        int inEnd      = n-1;
+        int postStart  = 0;
+        int postEnd    = n-1;
+        root           = buildBT(inorder, postorder, inStart, inEnd, postStart, postEnd);
+        return root;
     }
 };
