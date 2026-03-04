1cb25c68a805eacf31f882e4a29bd930b2ca213a
kanishkajain-17 <kanishkajn477@gmail.com>
2026-03-04 13:53:56 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (15.05%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+      void computeLPS(string pattern, vector<int>& lps) {
+        int M = pattern.length();
+        int len = 0;
+        lps[0] = 0;
+
+        int i = 1;
+        while (i < M) {
+            if (pattern[i] == pattern[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+    string shortestPalindrome(string s) {
+        
+        string rev = s;
+        reverse(begin(rev), end(rev));
+        string temp = s + "-" + rev;
+
+        vector<int> lps(temp.length(), 0);
+        computeLPS(temp, lps);
+
+        int longestLPSlength =  lps[temp.length() - 1];
+        string toAdd = rev.substr(0, s.length() - longestLPSlength);
+
+        return toAdd +  s;
+    }
+};
