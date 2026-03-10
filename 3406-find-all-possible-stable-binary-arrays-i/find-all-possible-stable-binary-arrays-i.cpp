554c928e9826efaa2eebd6df707232ba292c64e8
kanishkajain-17 <kanishkajn477@gmail.com>
2026-03-10 11:25:23 +0530

    Time: 229 ms (64.23%) | Memory: 8.8 MB (71.54%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -1,33 +1,42 @@
+//T.C : O(one * zero * limit)
+//S.C : O(one * zero)
 class Solution {
 public:
-    int numberOfStableArrays(int zero, int one, int limit) {
-        vector<vector<vector<long long>>> dp(
-            zero + 1, vector<vector<long long>>(one + 1, vector<long long>(2)));
-        long long mod = 1e9 + 7;
-        for (int i = 0; i <= min(zero, limit); i++) {
-            dp[i][0][0] = 1;
+    int M = 1e9 + 7;
+    int t[201][201][2];
+
+    int solve(int onesLeft, int zerosLeft, bool lastWasOne, int limit) {
+        if(onesLeft == 0 && zerosLeft == 0) {
+            return 1;
         }
-        for (int j = 0; j <= min(one, limit); j++) {
-            dp[0][j][1] = 1;
+
+        if(t[onesLeft][zerosLeft][lastWasOne] != -1) {
+            return t[onesLeft][zerosLeft][lastWasOne];
         }
-        for (int i = 1; i <= zero; i++) {
-            for (int j = 1; j <= one; j++) {
-                if (i > limit) {
-                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1] -
-                                  dp[i - limit - 1][j][1];
-                } else {
-                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
-                }
-                dp[i][j][0] = (dp[i][j][0] % mod + mod) % mod;
-                if (j > limit) {
-                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0] -
-                                  dp[i][j - limit - 1][0];
-                } else {
-                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0];
-                }
-                dp[i][j][1] = (dp[i][j][1] % mod + mod) % mod;
+
+
+        int result = 0;
+
+        if(lastWasOne == true) { //explore 0s
+            for(int len = 1; len <= min(zerosLeft, limit); len++) {
+                result = (result + solve(onesLeft, zerosLeft - len, false, limit)) % M;
+            }
+        } else { //explore 1s
+            for(int len = 1; len <= min(onesLeft, limit); len++) {
+                result = (result + solve(onesLeft - len, zerosLeft, true, limit)) % M;
             }
         }
-        return (dp[zero][one][0] + dp[zero][one][1]) % mod;
+
+        return t[onesLeft][zerosLeft][lastWasOne] = result;
+
+    }
+
+    int numberOfStableArrays(int zero, int one, int limit) {
+        memset(t, -1, sizeof(t));
+        int startWithOne = solve(one, zero, false, limit);
+        int startWithZero = solve(one, zero, true, limit);
+
+        return (startWithOne + startWithZero) % M;
+
     }
 };
