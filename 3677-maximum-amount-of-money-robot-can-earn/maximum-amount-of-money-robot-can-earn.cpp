e4d1ec0d62cc20059e37ad4daf7fc733f1e1aed0
kanishkajain-17 <kanishkajn477@gmail.com>
2026-04-03 01:10:59 +0530

    Time: 384 ms (72.62%) | Memory: 108.2 MB (88.38%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -5,8 +5,9 @@ class Solution {
 public:
     int m;
     int n;
+    int t[501][501][3];
 
-    int solve(vector<vector<int>>& coins, int i, int j, int neu, vector<vector<vector<int>>>& dp) {
+    int solve(vector<vector<int>>& coins, int i, int j, int neu) {
         if(i == m-1 && j == n-1) {
             if(coins[i][j] < 0 && neu > 0) {
                 return 0; //neutralize kardiya robber ko
@@ -19,23 +20,23 @@ public:
             return INT_MIN;
         }
 
-        if(dp[i][j][neu] != INT_MIN) {
-            return dp[i][j][neu];
+        if(t[i][j][neu] != INT_MIN) {
+            return t[i][j][neu];
         }
 
         //Take the current cell value
-        int take = coins[i][j] + max(solve(coins, i+1, j, neu, dp), solve(coins, i, j+1, neu, dp));
+        int take = coins[i][j] + max(solve(coins, i+1, j, neu), solve(coins, i, j+1, neu));
 
         //Skip current value if you can
         int skip = INT_MIN;
         if(coins[i][j] < 0 && neu > 0) {
-            int skipDown = solve(coins, i+1, j, neu-1, dp);
-            int skipRight = solve(coins, i, j+1, neu-1, dp);
+            int skipDown = solve(coins, i+1, j, neu-1);
+            int skipRight = solve(coins, i, j+1, neu-1);
 
             skip = max(skipDown, skipRight);
         }
 
-        return dp[i][j][neu] = max(take, skip);
+        return t[i][j][neu] = max(take, skip);
     }
 
     int maximumAmount(vector<vector<int>>& coins) {
@@ -43,8 +44,15 @@ public:
         m = coins.size();
         n = coins[0].size();
 
-vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(3, INT_MIN)));
-     return solve(coins, 0, 0, 2, dp);
+        for(int i = 0; i < 501; i++) {
+            for(int j = 0; j < 501; j++) {
+                for(int k = 0; k < 3; k++) {
+                    t[i][j][k] = INT_MIN;
+                }
+            }
+        }
+
+        return solve(coins, 0, 0, 2);
     }
 };
 
