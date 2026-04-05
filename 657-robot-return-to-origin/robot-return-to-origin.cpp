04ac97343cf65a6db216ecc3d78f103e486faa16
kanishkajain-17 <kanishkajn477@gmail.com>
2026-04-05 17:56:49 +0530

    Time: 3 ms (46.40%) | Memory: 10.3 MB (76.78%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int l = 0, r = 0, u = 0, d = 0;
+        for(int i = 0; i < moves.length(); i++){
+            if(moves[i] == 'L')
+                l++;
+            else if(moves[i] == 'R')
+                r++;
+            else if(moves[i] == 'U')
+                u++;
+            else
+                d++;
+        }
+       return l == r && u == d;
+    }
+};
