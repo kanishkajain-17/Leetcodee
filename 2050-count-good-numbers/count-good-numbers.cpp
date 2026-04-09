7e3e5625e6888a010ea3d011a9624d323b1df44a
kanishkajain-17 <kanishkajn477@gmail.com>
2026-04-09 07:29:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (96.60%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    const int M = 1e9 + 7;
+    int findPow(long long a, long long b){
+
+        if(b == 0)
+            return 1;
+
+        long long half = findPow(a, b / 2);
+        long long ans = (half * half) % M;
+
+        if(b % 2 == 1)
+            ans = (ans * a) % M;
+        return ans;
+    }
+    int countGoodNumbers(long long n) {
+        return (long long)findPow(5, (n + 1) / 2) * findPow(4, n / 2) % M;
+    }
+};
