a686fdb139e05ee38289136334187c2cd9841079
kanishkajain-17 <kanishkajn477@gmail.com>
2026-05-01 22:38:39 +0530

    Time: 16 ms (80.67%) | Memory: 90.1 MB (36.74%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,53 +1,36 @@
+//Approach (Using Median and Maths)
+//T.C : O(m*nlog(m*n))
+//S.C : O(m*n)
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        // Initialize an empty array to store all numbers from the grid
-        vector<int> numsArray;
-        int result = 0;
+        int m = grid.size();
+        int n = grid[0].size();
 
-        // Flatten the grid into numsArray
-        for (int row = 0; row < grid.size(); row++) {
-            for (int col = 0; col < grid[0].size(); col++) {
-                // If any element has a different remainder than the first
-                // element, it is impossible to make all elements equal, so
-                // return -1
-                if (grid[row][col] % x != grid[0][0] % x) return -1;
-                numsArray.push_back(grid[row][col]);
+        vector<int> vec;
+        //O(m*n)
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                vec.push_back(grid[i][j]);
             }
         }
 
-        // Sort numsArray in non-decreasing order to facilitate efficient
-        // operations
-        sort(numsArray.begin(), numsArray.end());
+        int L = vec.size(); //m*n
 
-        int length = numsArray.size();
-        int prefixIndex = 0;
-        int suffixIndex = length - 1;
+        //sort(begin(vec), end(vec)); //O(LlogL)
+        nth_element(begin(vec), begin(vec) + L/2, end(vec)); //O(L)
+        //{4, 2, 8, 6}
+        //{4, 2, 6, 8} - nth_element
+        //{2, 4, 6, 8} - complete sorting
 
-        // Gradually move prefixIndex and suffixIndex towards the middle
-        while (prefixIndex < suffixIndex) {
-            // If the prefix of equal elements is shorter than the suffix
-            if (prefixIndex < length - suffixIndex - 1) {
-                // Calculate the number of operations required to extend the
-                // prefix
-                int prefixOperations =
-                    (prefixIndex + 1) *
-                    (numsArray[prefixIndex + 1] - numsArray[prefixIndex]) / x;
-
-                result += prefixOperations;
-                // Move the prefix index forward
-                prefixIndex++;
-            } else {
-                // Calculate the number of operations required to extend the
-                // suffix
-                int suffixOperations =
-                    (length - suffixIndex) *
-                    (numsArray[suffixIndex] - numsArray[suffixIndex - 1]) / x;
-
-                result += suffixOperations;
-                // Move the suffix index backward
-                suffixIndex--;
+        int target = vec[L/2];
+        int result = 0;
+        for(int &num : vec) { //O(m*n)
+            if(num%x != target%x) {
+                return -1;
             }
+
+            result += abs(target-num)/x;
         }
 
         return result;
