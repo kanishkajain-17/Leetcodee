dd698bedb5fe3e53cd556ead0972b804e435561e
kanishkajain-17 <kanishkajn477@gmail.com>
2026-05-03 21:30:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.44%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -3,18 +3,13 @@ public:
     double solve(double x, long n){
         if(n == 0)
             return 1;
-        
         if(n < 0)
-            return solve(1.0 / x, -n);
-        
+            return solve(1 / x, -n);
         if(n % 2 == 0)
             return solve(x * x, n / 2);
-
-        return x * solve(x * x, (n - 1) / 2); //n is odd
-
-        
+        return x * solve(x * x, n / 2);
     }
     double myPow(double x, int n) {
-        return solve(x, long(n));
+        return solve(x, (long)n);
     }
 };
