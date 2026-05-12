35596d9379274e2e9df2d2490b22614054af100d
kanishkajain-17 <kanishkajn477@gmail.com>
2026-05-12 23:31:46 +0530

    Time: 84 ms (7.13%) | Memory: 50.7 MB (10.43%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -5,12 +5,12 @@ public:
         unordered_map<int, int> mp;
         int rank = 1;
 
-        for (auto &s : st) {
+        for (auto s : st) {
             mp[s] = rank++;
         }
 
         for(auto &i : arr) {
-            i = mp[i];
+            i = mp[i]; //i is a reference to actual array element toh value automatiaclly change ho jayegi 
         }
         return arr;
     }
