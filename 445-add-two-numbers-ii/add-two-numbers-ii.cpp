347abb086ccca116f263d4f02c3c3c21107c8168
kanishkajain-17 <kanishkajn477@gmail.com>
2026-05-30 18:24:36 +0530

    Time: 3 ms (50.91%) | Memory: 77 MB (53.92%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -10,6 +10,7 @@
  */
 class Solution {
 public:
+    //need to reverse the nodes 
     ListNode* reverse(ListNode* head) {
         
         ListNode* prev = NULL;
@@ -25,51 +26,79 @@ public:
         return prev;
         
     }
-    void insert(int x, ListNode* &head, ListNode* &temp) {
-        ListNode* node = new ListNode(x);
-        if(head == NULL) {
-            head = node;
-            temp = node;
-            return;
-        }
-        temp->next = node;
-        temp = node;
-    }
-    ListNode* add(ListNode* l1, ListNode* l2) {
-        int carry = 0;
-        ListNode* head = NULL;
-        ListNode* temp = NULL;
+    // void insert(int x, ListNode* &head, ListNode* &temp) {
+    //     ListNode* node = new ListNode(x);
+    //     if(head == NULL) {
+    //         head = node;
+    //         temp = node;
+    //         return;
+    //     }
+    //     temp->next = node;
+    //     temp = node;
+    // }
+    // ListNode* add(ListNode* l1, ListNode* l2) {
+    //     int carry = 0;
+    //     ListNode* head = NULL;
+    //     ListNode* temp = NULL;
 
-        while (l1 != NULL || l2 != NULL || carry != 0) {
+    //     while (l1 != NULL || l2 != NULL || carry != 0) {
+    //         sum = carry;
+            
+    //         if(l1 != NULL) {
+    //             sum += l1->val;
+    //             l1 = l1->next;
+    //         }
+    //         if(l2 != NULL) {
+    //             sum += l2->val;
+    //             l2 = l2->next;
+    //         }
 
-            int val1 = 0;
-            if(l1 != NULL)
-                val1 = l1->val;
-
-            int val2 = 0;
-            if(l2 != NULL)
-                val2 = l2->val;
-
-            int sum = val1 + val2 + carry;
-            int digit = sum % 10;
-            insert(digit, head, temp);
-
-            carry = sum / 10;
-
-            if(l1 != NULL)
-                l1 = l1->next;
-            if(l2 != NULL)
-                l2 = l2->next;
-        }
-        return head;
-    }
+            
+    //         val = sum % 10;
+    //         ListNode* newNode = new ListNode(val);
+    //         tail->next = newNode;
+    //         tail = newNode;
+            
+    //         carry = sum / 10;
+    //     }
+    //     return head;
+    // }
 
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         l1 = reverse(l1);
         l2 = reverse(l2);
 
-        ListNode* ans = add(l1, l2);
-        ans = reverse(ans);
-        return ans;        
+        // ListNode* ans = add(l1, l2);
+         ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+
+        int carry = 0;
+        int sum = 0;
+        int val = 0;
+        
+        while (l1 != NULL || l2 != NULL || carry != 0) {
+            sum = carry;
+            
+            if(l1 != NULL) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if(l2 != NULL) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            
+            val = sum % 10;
+            ListNode* newNode = new ListNode(val);
+            tail->next = newNode;
+            tail = newNode;
+            
+            carry = sum / 10;
+        }
+       // return dummy->next;
+       dummy = dummy->next;
+       dummy = reverse(dummy);
+        return dummy;        
     }
 };
