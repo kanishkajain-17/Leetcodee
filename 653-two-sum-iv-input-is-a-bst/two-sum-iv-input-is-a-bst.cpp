745c0fc1e5d2905f8bfafc34c062e63e20c16362
kanishkajain-17 <kanishkajn477@gmail.com>
2026-06-07 20:33:54 +0530

    Time: 0 ms (100.00%) | Memory: 37.1 MB (94.24%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -6,38 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void Inorder(TreeNode* root, vector<int>& ans) {
-        if (root == nullptr) {
+    vector<int> ans;
+    void inorder(TreeNode* root) {
+        if(!root)
             return;
-        }
-        Inorder(root->left,ans);
+        inorder(root->left);
         ans.push_back(root->val);
-        Inorder(root->right,ans);
-    }
-    bool check(vector<int> arr, int k) {
-        int start = 0;
-        int end = arr.size() - 1;
-        while (start < end) {
-            int sum = arr[start] + arr[end];
-            if (sum == k) {
-                return true;
-            } else if (sum > k) {
-                end--;
-            } else {
-                start++;
-            }
-        }
-        return false;
+        inorder(root->right);
     }
     bool findTarget(TreeNode* root, int k) {
-        vector<int> arr;
-        Inorder(root,arr);
-        return check(arr, k);
+        inorder(root);
+        int n = ans.size();
+        int l = 0;
+        int r = n - 1;
+        while (l < r) {
+            int sum = ans[l] + ans[r];
+            if(sum == k)
+                return true;
+            else if(sum > k)
+                r -= 1;
+            else
+                l += 1;
+        }
+        return false;
     }
 };
