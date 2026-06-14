f13e9169d2d52cb94aef7d41c14fcdfc40aaff25
kanishkajain-17 <kanishkajn477@gmail.com>
2026-06-14 23:49:52 +0530

    Time: 7 ms (9.09%) | Memory: 95 MB (94.40%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,16 +1,56 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> temp;
-        for (int i = 0; i < nums1.size(); i ++)
-            temp.push_back(nums1[i]);
-        for (int i = 0; i < nums2.size(); i ++)
-            temp.push_back(nums2[i]);
-        int n = temp.size();
-        sort(begin(temp), end(temp));
-        if(n % 2 == 1)
-            return temp[n / 2];
-        return (temp[(n / 2) - 1] + temp[n / 2]) / 2.0;
-        
+        int m = nums1.size();
+        int n = nums2.size();
+        int i = 0;
+        int j = 0;
+
+        int size = m + n;
+
+        int idx1 = (size - 1) / 2;
+        int ele1 = -1;
+
+        int idx2 = size / 2;;
+        int ele2 = 0;
+
+        int k = 0;
+
+        while (i < m && j < n) {
+            if(nums1[i] < nums2[j]) {
+                if(k == idx1) 
+                    ele1 = nums1[i];
+                if(k == idx2)
+                    ele2 = nums1[i];
+                i += 1;
+            }
+            else{
+                if(k == idx1) 
+                    ele1 = nums2[j];
+                if(k == idx2)
+                    ele2 = nums2[j];
+                j += 1;
+            }
+            k += 1;
+        }
+        while (i < m) {
+            if(k == idx1) 
+                ele1 = nums1[i];
+            if(k == idx2)
+                ele2 = nums1[i];
+            i += 1;
+            k += 1;
+        }
+        while (j < n) {
+            if(k == idx1) 
+                ele1 = nums2[j];
+            if(k == idx2)
+                ele2 = nums2[j];
+            j += 1;
+             k += 1;
+        }
+        if(size % 2 == 1)
+            return ele2;
+        return (ele1 + ele2) / 2.0;
     }
 };
