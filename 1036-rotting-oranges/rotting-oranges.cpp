f4100ee80145f1a5fac32c0321f21b118806d056
kanishkajain-17 <kanishkajn477@gmail.com>
2026-06-21 00:12:26 +0530

    Time: 7 ms (5.88%) | Memory: 17.2 MB (29.27%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,49 +1,54 @@
 class Solution {
 public:
-    typedef pair<int, int>P;
-    vector<vector<int>>directions{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    vector<vector<int>> directions{{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
+
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         int freshOranges = 0;
-        queue<P>que;
+        int minutes = 0;
+        
+        queue<pair<int, int>> que;
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 2)
-                    que.push({i, j});
-                else if(grid[i][j] == 1)
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if(grid[i][j] == 1)
                     freshOranges += 1;
+
+                else if(grid[i][j] == 2)
+                    que.push({i, j});
+
             }
         }
 
-        if(freshOranges == 0)
-            return 0;
+        while (!que.empty() && freshOranges != 0) {
 
-        int minutes = 0;
-        while(!que.empty()){
             int N = que.size();
-            
-            while(N--){
-                P curr = que.front();
+
+            while (N--) {
+
+                int x = que.front().first;
+                int y = que.front().second;
                 que.pop();
 
-                int i = curr.first;
-                int j = curr.second;
+                for (auto &dir : directions) {
 
-                for(auto & dir : directions){
-                    int new_i = dir[0] + i;
-                    int new_j = dir[1] + j;
+                    int new_x = x + dir[0];
+                    int new_y = y + dir[1];
 
-                    if(new_i >= 0 && new_i < m && new_j >= 0 && new_j < n && grid[new_i][new_j] == 1){
-                        grid[new_i][new_j] = 2;
-                        que.push({new_i, new_j});
-                        freshOranges--;
+                    if((new_x >= 0 && new_x < m) && (new_y >= 0 && new_y < n) && grid[new_x][new_y] == 1) {
+
+                        que.push({new_x, new_y});
+                        grid[new_x][new_y] = 2;
+                         freshOranges -= 1;
                     }
                 }
             }
-            minutes++;
+           
+            minutes += 1;
         }
-        return (freshOranges == 0) ? minutes - 1 : -1;
+
+        return freshOranges == 0 ? minutes : -1;
     }
 };
