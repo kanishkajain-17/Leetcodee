fda64467d78276ec5c4e29ac6aed303a3434eb77
kanishkajain-17 <kanishkajn477@gmail.com>
2026-06-21 18:00:32 +0530

    Time: 56 ms (61.30%) | Memory: 21.2 MB (43.43%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    //bfs hard problem
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        queue<pair<string, int>>q;
-        unordered_set<string>st(wordList.begin(), wordList.end());
+        
+        queue<pair<string, int>> q;
+        unordered_set<string> st(begin(wordList), end(wordList));
+
         q.push({beginWord, 1});
         st.erase(beginWord);
-        while(!q.empty()){
+
+        while (!q.empty()) {
+
             string word = q.front().first;
             int steps = q.front().second;
             q.pop();
 
             if(word == endWord)
                 return steps;
+            
+            for (int i = 0; i < word.size(); i ++) {
 
-            for(int i = 0; i < word.size(); i++){
                 char original = word[i];
-                for(char ch = 'a'; ch <= 'z'; ch++){
+
+                for (char ch = 'a'; ch <= 'z'; ch++) {
                     word[i] = ch;
-                    //if exists in the set
-                    if(st.find(word) != st.end()){
-                        st.erase(word);
+                    if(st.find(word) != st.end()) {
+
                         q.push({word, steps + 1});
+                        st.erase(word);
                     }
                 }
+
                 word[i] = original;
             }
         }
