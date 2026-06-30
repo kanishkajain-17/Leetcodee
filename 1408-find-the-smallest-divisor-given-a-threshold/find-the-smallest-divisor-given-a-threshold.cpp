d7b10df3157bb5593e3a5b311ee33daf4a15433d
kanishkajain-17 <kanishkajn477@gmail.com>
2026-06-30 21:34:11 +0530

    Time: 9 ms (46.43%) | Memory: 26.1 MB (80.25%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    bool canDivide(vector<int>& nums, int threshold, int mid) {
-        long long sum = 0;
-        for (int &x : nums) {
-            sum += ceil((double)x / mid);
+    bool canDivide(vector<int>& nums, int mid, int threshold) {
+
+        int sum = 0;
+
+        for (int i = 0;i < nums.size(); i ++) {
+
+            sum += ceil((double)nums[i] / mid);
         }
         return sum <= threshold;
     }
     int smallestDivisor(vector<int>& nums, int threshold) {
         int n = nums.size();
-        int l = 1;
-        int r = *max_element(begin(nums), end(nums));
-        while (l < r) {
+        int l = 1, r = *max_element(begin(nums), end(nums));
+        int ans = 0;
+
+        while (l <= r) {
+
             int mid = l + (r - l) / 2;
-            if(canDivide(nums, threshold, mid) == true)
-                r = mid;
-            
+
+            if(canDivide(nums, mid, threshold)) {
+                ans = mid;
+                r = mid - 1;
+            }
             else
                 l = mid + 1;
         }
-        return r;
+        return ans;
     }
 };
