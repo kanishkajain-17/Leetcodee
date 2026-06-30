d37afac65899faee1ca106ec36d70e2692951cf9
kanishkajain-17 <kanishkajn477@gmail.com>
2026-06-30 23:13:31 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (60.82%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int findKthPositive(vector<int>& arr, int k) {
-        int n = arr.size();
-        int l = 0, r= n - 1;
+    int findKthPositive(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+
         while (l <= r) {
-            int mid = l + (r - l) /2;
-            int missing = arr[mid] - (mid + 1);
-            if(missing < k) //mtlb aur number aayenge abhi jo aapni position par nahi h
+            int mid = l + (r - l) / 2;
+
+            if(nums[mid] - (mid + 1) < k)
                 l = mid + 1;
             else
-                r = mid - 1;//jyda number aagaye
+                r = mid - 1;
         }
         return l + k;
-        //l + k -> l number toh already present the aab k number or chaiye toh simply add krdo bcz input is in sorted order
     }
 };
