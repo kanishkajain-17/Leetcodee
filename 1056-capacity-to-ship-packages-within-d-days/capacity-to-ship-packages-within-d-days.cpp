b9fe1b97d9878787f383475cb6ebbeb9b36414fb
kanishkajain-17 <kanishkajn477@gmail.com>
2026-06-30 21:54:20 +0530

    Time: 15 ms (31.78%) | Memory: 35.1 MB (53.91%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
-    int canShip(vector<int> &weights, int mid, int day) {
+    bool canShip(vector<int>& weights, int mid, int days) {
+
         int sum = 0;
-        int actualDay = 1;
-        for (int &x : weights) {
+        int current_days = 1;
+        int n = weights.size();
+        for (int i = 0; i < n; i++) {
 
-            if(sum + x > mid) {
-                actualDay += 1;
-                sum = x;
+            sum += weights[i];
+
+            if(sum > mid) {
+                current_days += 1;
+                sum = weights[i];
             }
-            else
-                sum += x;
         }
-
-        return actualDay;
+        return current_days <= days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
+         int n = weights.size();
         int l = *max_element(begin(weights), end(weights));
         int r = accumulate(begin(weights), end(weights), 0);
+        int ans = 0;
 
-        while (l < r) {
+        while (l <= r) {
+
             int mid = l + (r - l) / 2;
-            if(canShip(weights, mid, days) <= days) //to isse aur choti value nikalne ka try kro 
-                r = mid;
+
+            if(canShip(weights, mid, days)) {
+                ans = mid;
+                r = mid - 1;
+            }
             else
                 l = mid + 1;
         }
-        return r;
+        return ans;
     }
 };
