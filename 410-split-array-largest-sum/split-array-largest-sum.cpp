06792f0ca656c5c9a324300fdea0cd661c0339c9
kanishkajain-17 <kanishkajn477@gmail.com>
2026-07-01 23:49:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (10.35%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-    bool canSplit(vector<int>& nums, int k, int mid) {
+    bool canSplit(vector<int>& arr, int mid, int k) {
+
+        int n = arr.size();    
+        int subarray_count = 1;
         int sum = 0;
-        int subarrayCount = 1;
 
-        for (int &x : nums) {
-            if(sum + x > mid){
-                subarrayCount += 1;
-                sum = x;
+        for (int i = 0; i < n; i ++) {
+
+            sum += arr[i];
+            if(sum > mid) {
+                subarray_count += 1;
+                sum = arr[i];
             }
-            else
-                sum += x;
         }
-        return subarrayCount <= k;
+        return subarray_count <= k;
     }
-    int splitArray(vector<int>& nums, int k) {
-        int n = nums.size();
-
-        int l = *max_element(begin(nums), end(nums));
-        int r = accumulate(begin(nums), end(nums), 0);
+    int splitArray(vector<int>& arr, int k) {
+        
+        int n = arr.size();
+        
+        int l = *max_element(begin(arr), end(arr));
+        int r = accumulate(begin(arr), end(arr), 0);
         int ans = 0;
-
+        
         while (l <= r) {
+            
             int mid = l + (r - l) / 2;
-            if(canSplit(nums, k, mid)) {
+            
+            if(canSplit (arr, mid, k)) {
                 ans = mid;
                 r = mid - 1;
             }
