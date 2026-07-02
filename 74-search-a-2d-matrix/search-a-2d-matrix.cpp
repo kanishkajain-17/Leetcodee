0bfc7510bd262b2523f921aa02d2b5762caedbc9
kanishkajain-17 <kanishkajn477@gmail.com>
2026-07-02 13:02:37 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (78.88%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    // i starts at 0 and can go up to m-1 → at most m moves.
-    // j starts at n-1 and can go down to 0 → at most n moves.
-    // tc = O(m + n)
-    // sc = O(1)
-    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
         int m = matrix.size();
         int n = matrix[0].size();
-        int i = 0, j = n - 1;
+        int l = 0;
+        int r = m * n - 1;
 
-        while (i < m && j >= 0) {
-            if(matrix[i][j] == target)
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int val = matrix[mid / n][mid % n];
+
+            if(val == target)
                 return true;
-            else if(matrix[i][j] > target)
-                j -= 1;
+            else if(val > target)
+                r = mid - 1;
             else
-                i += 1;
+                l = mid + 1;
         }
         return false;
     }
