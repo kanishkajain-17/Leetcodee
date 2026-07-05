896fb2ae47e5320e93d48d02ce170668df50d367
kanishkajain-17 <kanishkajn477@gmail.com>
2026-07-05 23:16:01 +0530

    Time: 1 ms (23.16%) | Memory: 9.3 MB (11.35%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int n = s.length();
-        long long  ans = 0;
-        int sign = 1;
-        int i  = 0;
         
-        if(s[i] == '.')
-            return (int)ans;
-
-        while(i < n && s[i] == ' '){
-            i++; 
-        }
-        if(s[i] == '+' || s[i] == '-'){
-            if(s[i] == '-'){
+        int n = s.length();
+        int i = 0;
+        int sign = 1;
+        long long ans = 0;
+        while (i < n && s[i] == ' ') 
+            i += 1;
+        
+        if(s[i] == '-' || s[i] == '+') {
+            if(s[i] == '-')
                 sign = -1;
-            }
-            i++;
+            i += 1;
         }
-        while(i < n && (s[i] >= '0' && s[i] <= '9')){
 
-            ans = (ans * 10 ) + (s[i] - '0'); 
+        while (i < n && s[i] == '0')
+            i += 1;
+        while (i < n && s[i] >= '0' && s[i] <= '9') {
 
-            if(sign == 1 && ans > INT_MAX)
-                 return INT_MAX;
-            if(sign == -1 && -ans < INT_MIN)
-                 return INT_MIN;
-            i++;
+            ans = (s[i] - '0') + (ans * 10);
+            i += 1;
+
+             if(sign == 1 && ans > INT_MAX)
+                return INT_MAX;
+
+            if(sign == - 1 && (-ans) < INT_MIN)
+                return INT_MIN;
         }
+
+       
         
         return (int)(ans * sign);
     }
