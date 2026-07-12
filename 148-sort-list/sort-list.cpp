dbab4ad19d39c9c059652b9d8402c57c023a0de2
kanishkajain-17 <kanishkajn477@gmail.com>
2026-07-12 18:06:51 +0530

    Time: 12 ms (74.89%) | Memory: 58.2 MB (62.23%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,37 +10,49 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoSortedLists(ListNode* l1, ListNode* l2) {
+    ListNode* mergeLists(ListNode *l1, ListNode *l2) {
+
         if(!l1)
             return l2;
+        
         if(!l2)
             return l1;
+
         if(l1->val <= l2->val) {
-            l1->next = mergeTwoSortedLists(l1->next, l2); //bcz l1 ka ek use hogaya aab l1 mai next value se compare hoga l2 
+
+            l1->next = mergeLists(l1->next, l2);
             return l1;
         }
         else {
-            l2->next = mergeTwoSortedLists(l1, l2->next);
+
+            l2->next = mergeLists(l1, l2->next);
             return l2;
         }
         return NULL;
     }
+
     ListNode* sortList(ListNode* head) {
+        
         if(head == NULL || head->next == NULL)
             return head;
-        ListNode* slow = head;
-        ListNode* fast = head;
-        ListNode* prev = NULL;
+
+        ListNode *prev = NULL;
+        ListNode *slow = head;
+        ListNode *fast = head;
 
         while (fast != NULL && fast->next != NULL) {
+
             prev = slow;
             slow = slow->next;
             fast = fast->next->next;
-        }
-        prev->next = NULL;
-        ListNode* l1 = sortList(head);
-        ListNode* l2 = sortList(slow);
 
-        return mergeTwoSortedLists(l1, l2);
+        }
+
+        prev->next = NULL;
+
+        ListNode *l1 = sortList(head);
+        ListNode *l2 = sortList(slow);
+
+        return mergeLists(l1, l2);
     }
 };
