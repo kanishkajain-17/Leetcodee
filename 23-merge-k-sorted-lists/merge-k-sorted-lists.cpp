84f1d023cc664c53ce91fd0481092255cf5bdec3
kanishkajain-17 <kanishkajn477@gmail.com>
2026-07-21 21:22:48 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (38.49%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,34 +10,37 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoSortedLists(ListNode* l1, ListNode* l2) {
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+
         if(!l1)
             return l2;
         if(!l2)
             return l1;
-        if(l1->val <= l2->val) {
-            l1->next = mergeTwoSortedLists(l1->next, l2);
+
+        if(l1->val < l2->val) {
+
+            l1->next = merge(l1->next, l2);
             return l1;
         }
         else {
-            l2->next = mergeTwoSortedLists(l1, l2->next);
+
+            l2->next = merge(l1, l2->next);
             return l2;
         }
         return NULL;
     }
+    ListNode* partition(vector<ListNode*>& lists, int start, int end) {
 
-    ListNode* partitionAndMerge(int start, int end, vector<ListNode*>& lists) {
-
-        if(start == end) //only one node is there 
+        if(start == end)
             return lists[start];
         
         int mid = start + (end - start) / 2;
-        ListNode* L1 = partitionAndMerge(start, mid, lists);
-        ListNode* L2 = partitionAndMerge(mid + 1, end, lists);
 
-        return mergeTwoSortedLists(L1, L2);
+        ListNode* l1 = partition(lists, start, mid);
+        ListNode* l2 = partition(lists, mid + 1, end);
 
-
+        return merge(l1, l2);
+       
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
@@ -46,6 +49,6 @@ public:
         if(n == 0)
             return NULL;
         
-        return partitionAndMerge(0, n - 1, lists);
+        return partition(lists, 0, n - 1);
     }
 };
