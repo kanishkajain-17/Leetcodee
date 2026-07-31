7d58530477a66c511dafc71e9d85b25526dba35f
kanishkajain-17 <kanishkajn477@gmail.com>
2026-07-31 07:37:32 +0530

    Time: 9 ms (53.64%) | Memory: 25.5 MB (72.47%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> mp(26, 0);
+        int ans = 0;
+
+        for (char &ch : word)
+            mp[ch - 'a'] += 1;
+
+        sort(begin(mp), end(mp), greater<int>());
+
+        for (int i = 0; i < 26; i ++) {
+
+            int freq = mp[i];
+            int press = i / 8 + 1;
+            ans += freq * press;
+        }
+        return ans;
+    }
+};
