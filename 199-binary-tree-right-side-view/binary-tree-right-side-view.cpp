04c429de2c649fd7c3244651e1125b59dfaddda0
kanishkajain-17 <kanishkajn477@gmail.com>
2026-08-04 19:57:06 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (22.15%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,77 +11,33 @@
  */
 class Solution {
 public:
-    void preOrder(TreeNode* root, int level, vector<int>& ans){
-        if(!root)
-            return;
-        if(ans.size() < level)
-            ans.push_back(root->val);
-        preOrder(root->right, level + 1, ans);
-        preOrder(root->left, level + 1, ans);
-    }
-
     vector<int> rightSideView(TreeNode* root) {
-        // if(!root)
-        //     return {};
-        // vector<int>ans;
-        // queue<pair<TreeNode*,pair<int, int>>>q; //node, level
-        // q.push({root, {0, 0}});
-        // map<int, int>mp; //vertical axis coordinate, node value
-        
-        // while(!q.empty()){
-        //     auto it = q.front();
-        //     q.pop();
-            
-        //     TreeNode* node = it.first;
-        //     int vertical = it.second.first;
-        //     int level = it.second.second;
-            
-        //     if(mp.find(level) == mp.end())
-        //         mp[level] = node->val;
-                      
-        //     if(node->right != NULL)
-        //         q.push({node->right, {vertical + 1, level + 1}});
-        //     if(node->left != NULL)
-        //         q.push({node->left, {vertical - 1, level + 1}});
-        // }
-        // for(auto it : mp)
-        //     ans.push_back(it.second);
-        // return ans;
-
-
-        //USING BFS---------->>
-
-        // if(!root)
-        //     return {};
-        // vector<int>ans;
-        // queue<TreeNode*>que;
-        
-        // que.push(root);
-        // while(!que.empty()){
-        //     TreeNode * node = NULL;
-        //     int N = que.size();
-            
-        //     while(N--){
-        //         node = que.front();
-        //         que.pop();
-                
-        //         if(node->left != NULL)
-        //             que.push(node->left);
-
-        //         if(node->right != NULL)
-        //             que.push(node->right);
-        //     }
-        //     ans.push_back(node->val);
-        // }
-        // return ans;
-
-
-
-        //USING DFS----------->>
         if(!root)
             return {};
-        vector<int>ans;
-        preOrder(root, 1, ans);
+            
+        queue<TreeNode*> que;
+        que.push(root);
+
+        vector<int> ans;
+
+        while (!que.empty()) {
+
+            TreeNode* node = NULL;
+            int N = que.size();
+
+            while (N--) {
+
+                node = que.front();
+                que.pop();
+
+                if(node->left != NULL)
+                    que.push(node->left);
+
+                if(node->right != NULL)
+                    que.push(node->right);
+            }
+            ans.push_back(node->val);
+        }
         return ans;
     }
 };
